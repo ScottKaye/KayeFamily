f2d01325bc43fe1fa820416aa565f77ed9c4aeca
Scott Kaye <pgreviews@gmail.com>
2016-10-31 15:24:42 -0400

    Basic MVC

---
+++ src/KayeFamily/Areas/ScottKayeNet/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KayeFamily.Areas.ScottKayeNet.Controllers
+{
+	[Area("ScottKayeNet")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/KayeFamily/Startup.cs
+++ src/KayeFamily/Startup.cs
@@ -8,19 +8,31 @@ namespace KayeFamily
 {
 	public class Startup
 	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services) { }
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
 		{
-			loggerFactory.AddConsole();
+			loggerFactory.AddConsole(LogLevel.Warning);
 
 			if (env.IsDevelopment())
 				app.UseDeveloperExceptionPage();
 
-			app.Run(async context => { await context.Response.WriteAsync("Hello World!"); });
+			app.UseStaticFiles();
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "areas",
+					template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+				);
+
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}"
+				);
+			});
 		}
 	}
 }
